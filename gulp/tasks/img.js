e5d9e6d288af0f32a27842a4885ce1bddaad2080
Nick Dobrynin <dobryninnick1@gmail.com>
2021-12-24 01:15:26 +0600

    Fix

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -10,21 +10,16 @@ export const img = () => {
             })
         ))
         .pipe(app.plugins.newer(app.path.build.img))
-        .pipe(app.plugins.if(app.isBuild, webp()))
-        .pipe(app.plugins.if(app.isBuild, app.gulp.dest(app.path.build.img)))
-        .pipe(app.plugins.if(app.isBuild, app.gulp.src(app.path.src.img)))
-        .pipe(app.plugins.if(app.isBuild, app.plugins.newer(app.path.build.img)))
-        .pipe(
-            app.plugins.if(
-                app.isBuild,
-                imagemin({
-                    progressive:        true,
-                    svgoPlugins:        [{removeViewBox: false}],
-                    interlaced:         true,
-                    optimizationLevel:  3 // 0 to 7
-                })
-            )
-        )
+        .pipe(webp())
+        .pipe(app.gulp.dest(app.path.build.img))
+        .pipe(app.gulp.src(app.path.src.img))
+        .pipe(app.plugins.newer(app.path.build.img))
+        .pipe(imagemin({
+            progressive:        true,
+            svgoPlugins:        [{removeViewBox: false}],
+            interlaced:         true,
+            optimizationLevel:  3 // 0 to 7
+        }))
         .pipe(app.gulp.dest(app.path.build.img))
         .pipe(app.gulp.src(app.path.src.svg))
         .pipe(app.gulp.dest(app.path.build.img))
